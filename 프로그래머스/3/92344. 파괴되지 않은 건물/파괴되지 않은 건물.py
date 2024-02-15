719a0f1ac97eaae2c85e545c8c6cd8972d79d28c
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-02-15 15:35:27 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 726.48 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,9 +1,9 @@
-
 def solution(board, skill):
     answer = 0
-    tmp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)] # 누적합 기록을 위한 배열
+    tmp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
+    # 누적합 기록을 위한 배열
     for type, r1, c1, r2, c2, degree in skill:
-        # 누적합 기록, 부호에 주의할 것
+    # 누적합 기록, 부호에 주의할 것
         tmp[r1][c1] += degree if type == 2 else -degree
         tmp[r1][c2 + 1] += -degree if type == 2 else degree
         tmp[r2 + 1][c1] += -degree if type == 2 else degree
@@ -20,6 +20,6 @@ def solution(board, skill):
     for i in range(len(board)):
         for j in range(len(board[i])):
             board[i][j] += tmp[i][j]
-            # board에 값이 1이상인 경우 answer++
+            # board에 값이 1이상인 경우 answer ++
             if board[i][j] > 0: answer += 1
     return answer
